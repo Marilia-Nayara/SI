858f0ad1880e3a521b86f0aa202546ebc1a9fb66
Patricia <Patricia@Patricia-PC>
2016-06-07 16:27:51 -0300

    negocio compra

---
+++ src/Negocios/NegociosCompra.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Negocios;
+
+import Class.Compra;
+import Utilitarios.BancoCompra;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+public class NegociosCompra {
+    public int salvar(int cpf_c, Timestamp data, boolean isEntrega, float preco_final, List<Integer> lista_prod, List<Integer> quantidades) {
+        Compra compra = new Compra();
+        BancoCompra bc = new BancoCompra(); 
+        compra.setCpf_c(cpf_c);
+        compra.setData(data);
+        compra.setIsEntrega(isEntrega);
+        compra.setPreco_final(preco_final);
+        compra.setLista_prod(lista_prod);
+        compra.setQuantidades(quantidades);
+        int codigo = bc.inserirCompra(compra);
+        return codigo;
+    }
+    public Compra ConsultarCompra(int codigo){
+        BancoCompra bc = new BancoCompra(); 
+        return bc.ConsultarCompra(codigo);
+    }
+}
