05e47a40deb21e89751790aee1604d8b302b53a6
Marilia-Nayara <marilianayara@gmail.com>
2016-06-10 21:55:46 -0300

    Add files via upload

---
+++ src/Class/Entrega.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author  Marilia Nayara, Gustavo Almeida, Patricia Lopes, Eddie
+ */
+public class Entrega {
+    private String dia;
+    private int quant;
+
+    public Entrega() {
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+    
+    
+}
