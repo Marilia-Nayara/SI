1c40cfb33d8a9b72a137c098c578e1f1f3de3eaf
Marilia-Nayara <marilianayara@gmail.com>
2016-06-11 15:02:43 -0300

    faltava as importações por isso estava com erro

--- src/Utilitarios/BancoCompra.java
+++ src/Utilitarios/BancoCompra.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import oracle.net.aso.b;
+
        
 
 public class BancoCompra {
