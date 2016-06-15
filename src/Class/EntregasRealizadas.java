952cf056fb8e995423b2e710cbbfb4c56842509b
Marilia-Nayara <marilianayara@gmail.com>
2016-06-14 22:50:52 -0300

    class de entreda realizada

---
+++ src/Class/EntregasRealizadas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ *
+ * @author  Marilia Nayara, Gustavo Almeida, Patricia Lopes, Eddie
+
+ */
+public class EntregasRealizadas {
+    private int quant;
+    private Timestamp datas;
+
+    
+
+    
+}
